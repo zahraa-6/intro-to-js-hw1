808be7796e2d180a3c16c85cb70d70ef81425ab1
zahraa-6 <za998916@gmail.com>
2024-05-11 15:15:44 -0700

    js1

---
+++ app.js
@@ -0,0 +1,17 @@
+var myName='zahraa'
+console.log("yourname",myName);
+var myAge=25
+console.log("your age",myAge);
+var student=true
+console.log("you are student",student);
+
+var myName='zahraa'
+console.log(myName,typeof myName);
+
+var myAge=25
+console.log("your age" ,typeof myAge);
+var student=true 
+console.log("you are student",typeof student);
+var greeting="hello"
+var name1="hello"
+console.log(":)" , greeting+"-" , name1);
